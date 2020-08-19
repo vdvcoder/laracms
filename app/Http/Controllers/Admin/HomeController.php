cd23bde9f6d3e428d1b9ee43477d70eb2716fa5b
Laravel Daily <info@laraveldaily.com>
2020-08-19 07:52:40 +0000

    QuickAdminPanel automatic commit

---
+++ app/Http/Controllers/Admin/HomeController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use LaravelDaily\LaravelCharts\Classes\LaravelChart;
+
+class HomeController
+{
+    public function index()
+    {
+        $settings1 = [
+            'chart_title'           => '# Users',
+            'chart_type'            => 'number_block',
+            'report_type'           => 'group_by_date',
+            'model'                 => 'App\\User',
+            'group_by_field'        => 'email_verified_at',
+            'group_by_period'       => 'day',
+            'aggregate_function'    => 'count',
+            'filter_field'          => 'created_at',
+            'group_by_field_format' => 'd/m/Y H:i:s',
+            'column_class'          => 'col-md-3',
+            'entries_number'        => '5',
+        ];
+
+        $settings1['total_number'] = 0;
+
+        if (class_exists($settings1['model'])) {
+            $settings1['total_number'] = $settings1['model']::when(isset($settings1['filter_field']), function ($query) use ($settings1) {
+                if (isset($settings1['filter_days'])) {
+                    return $query->where(
+                        $settings1['filter_field'],
+                        '>=',
+                        now()->subDays($settings1['filter_days'])->format('Y-m-d')
+                    );
+                } else if (isset($settings1['filter_period'])) {
+                    switch ($settings1['filter_period']) {
+                        case 'week':
+                            $start  = date('Y-m-d', strtotime('last Monday'));
+                            break;
+                        case 'month':
+                            $start = date('Y-m') . '-01';
+                            break;
+                        case 'year':
+                            $start  = date('Y') . '-01-01';
+                            break;
+                    }
+
+                    if (isset($start)) {
+                        return $query->where($settings1['filter_field'], '>=', $start);
+                    }
+                }
+            })
+                ->{$settings1['aggregate_function'] ?? 'count'}($settings1['aggregate_field'] ?? '*');
+        }
+
+        $settings2 = [
+            'chart_title'        => '# Paid Orders',
+            'chart_type'         => 'bar',
+            'report_type'        => 'group_by_string',
+            'model'              => 'App\\Order',
+            'group_by_field'     => 'pait_at',
+            'aggregate_function' => 'count',
+            'filter_field'       => 'created_at',
+            'filter_days'        => '7',
+            'column_class'       => 'col-md-6',
+            'entries_number'     => '5',
+        ];
+
+        $chart2 = new LaravelChart($settings2);
+
+        $settings3 = [
+            'chart_title'           => '# Products',
+            'chart_type'            => 'number_block',
+            'report_type'           => 'group_by_date',
+            'model'                 => 'App\\Product',
+            'group_by_field'        => 'created_at',
+            'group_by_period'       => 'day',
+            'aggregate_function'    => 'count',
+            'filter_field'          => 'created_at',
+            'group_by_field_format' => 'd/m/Y H:i:s',
+            'column_class'          => 'col-md-3',
+            'entries_number'        => '5',
+        ];
+
+        $settings3['total_number'] = 0;
+
+        if (class_exists($settings3['model'])) {
+            $settings3['total_number'] = $settings3['model']::when(isset($settings3['filter_field']), function ($query) use ($settings3) {
+                if (isset($settings3['filter_days'])) {
+                    return $query->where(
+                        $settings3['filter_field'],
+                        '>=',
+                        now()->subDays($settings3['filter_days'])->format('Y-m-d')
+                    );
+                } else if (isset($settings3['filter_period'])) {
+                    switch ($settings3['filter_period']) {
+                        case 'week':
+                            $start  = date('Y-m-d', strtotime('last Monday'));
+                            break;
+                        case 'month':
+                            $start = date('Y-m') . '-01';
+                            break;
+                        case 'year':
+                            $start  = date('Y') . '-01-01';
+                            break;
+                    }
+
+                    if (isset($start)) {
+                        return $query->where($settings3['filter_field'], '>=', $start);
+                    }
+                }
+            })
+                ->{$settings3['aggregate_function'] ?? 'count'}($settings3['aggregate_field'] ?? '*');
+        }
+
+        return view('home', compact('settings1', 'chart2', 'settings3'));
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,45 @@
+<?php
+
+Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', 'middleware' => ['auth:api']], function () {
+    // Permissions
+    Route::apiResource('permissions', 'PermissionsApiController');
+
+    // Roles
+    Route::apiResource('roles', 'RolesApiController');
+
+    // Users
+    Route::apiResource('users', 'UsersApiController');
+
+    // Orders
+    Route::apiResource('orders', 'OrdersApiController');
+
+    // Coupons
+    Route::apiResource('coupons', 'CouponsApiController');
+
+    // Content Categories
+    Route::apiResource('content-categories', 'ContentCategoryApiController');
+
+    // Content Tags
+    Route::apiResource('content-tags', 'ContentTagApiController');
+
+    // Content Pages
+    Route::post('content-pages/media', 'ContentPageApiController@storeMedia')->name('content-pages.storeMedia');
+    Route::apiResource('content-pages', 'ContentPageApiController');
+
+    // Faq Categories
+    Route::apiResource('faq-categories', 'FaqCategoryApiController');
+
+    // Faq Questions
+    Route::apiResource('faq-questions', 'FaqQuestionApiController');
+
+    // Product Categories
+    Route::post('product-categories/media', 'ProductCategoryApiController@storeMedia')->name('product-categories.storeMedia');
+    Route::apiResource('product-categories', 'ProductCategoryApiController');
+
+    // Product Tags
+    Route::apiResource('product-tags', 'ProductTagApiController');
+
+    // Products
+    Route::post('products/media', 'ProductApiController@storeMedia')->name('products.storeMedia');
+    Route::apiResource('products', 'ProductApiController');
+});
